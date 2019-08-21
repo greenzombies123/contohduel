ca63e71788823af909a70110907c2782746b1a35
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-21 12:08:16 +0700

    Add files via upload

---
+++ contohduel.sh
@@ -0,0 +1,97 @@
+
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " DUEL " |lolcat
+
+echo $green"---------------------------------------"
+sleep 1
+toilet -f slant "HALO" |lolcat
+toilet -f slant "BANGSAT" |lolcat
+toilet -f slant "APAKAH" |lolcat
+toilet -f slant "ANDA" |lolcat
+toilet -f slant "YAKIN" |lolcat
+toilet -f slant "MAU" |lolcat
+toilet -f slant "DUEL" |lolcat
+toilet -f slant "DENGAN" |lolcat
+toilet -f slant "TUAN" |lolcat
+toilet -f slant "SAYA" |lolcat
+toilet -f slant "GREEN" |lolcat
+toilet -f slant "ZOMBIES" |lolcat
+toilet -f slant "APABILA" |lolcat
+toilet -f slant "SETUJU" |lolcat
+toilet -f slant "TUNGGU" |lolcat
+toilet -f slant "DUA" |lolcat
+toilet -f slant "MENIT" |lolcat
+toilet -f slant "ANDA" |lolcat
+toilet -f slant "AKAN" |lolcat
+toilet -f slant "DIARAHKAN" |lolcat
+toilet -f slant "KE ARENA" |lolcat
+toilet -f slant "DUEL" |lolcat
+toilet -f slant "SILAHKAN" |lolcat
+toilet -f slant "TUNGGU" |lolcat
+echo $green"DUEL GANAS"
+sleep 1
+echo $red"PERATURAN DUEL"
+echo $white"---------------------------------------"
+echo $green"TUAN GREEN ZOMBIES MENUNGGU ANDA       "
+echo $red"      JANGAN TAKUT ANJING:V              "
+echo $white"---------------------------------------"
+echo $purple" CARA DUEL AKAN DI JELASKAN           "
+echo $white"---------------------------------------"
+sleep 2
+echo $blue"begini ya cara duel nya"
+echo $green"---------------------------------------"
+echo $blue"anda akan di tantang untuk saling mengacak acak data tuan saya"
+sleep 1
+echo $green"---------------------------------------"
+echo $blue"yang bisa mengacak ngacak data terlebih dahulu dia pemenang nya"
+sleep 1
+echo $green"---------------------------------------"
+echo $green"yang ke dua anda di tantang untuk adu deface"
+sleep 1
+echo $green"---------------------------------------"
+echo $green"yang ke tiga anda di tantang untuk saling lacak lokasi"
+sleep 1
+echo $green"---------------------------------------"
+echo $green"apabila anda takut lebih baik tidur saja"
+sleep 1
+echo $green"---------------------------------------"
+echo $green"apabila berani tunggu dua menit"
+sleep 8
+echo $green"tools akan diinstall"
+echo $green"---------------------------------------"
+toilet -f slant "tools" |lolcat
+toilet -f slant "akan" |lolcat
+toilet -f slant "diinstall" |lolcat
+echo -e "TUNGGU SEBENTAR"
+sleep 2
+echo -e "LAGI INSTALL ALAT NYA"
+sleep 1
+echo -e "LOADING INSTALL AOXDEFACE"
+sleep 1
+echo -e "LOADING INSTALL NMAP"
+sleep 1
+echo -e "LOADING INSTALL GNMAP EXTRA"
+sleep 1
+echo -e "LOADING INSTALL GREEN ZOMBIES DUEL"
+sleep 3
+
+echo $green"---------------------------------------"
+toilet -f slant "DUEL" |lolcat
+echo $green"---------------------------------------"
+echo -e "GREEN ZOMBIES DUEL SUDAH TERINSTALL"
+sleep 1
+echo -e "BUKA TAB BARU"
+sleep 1
+echo -e "KETIK sh greenzombies.sh"
+sleep 1
+echo -e "HARAP DI PERCEPAT"
+sleep 5
+echo -e "TUAN SAYA GREEN ZOMBIES SEDANG MENUNGGU"
+toilet -f slant "DUEL"
